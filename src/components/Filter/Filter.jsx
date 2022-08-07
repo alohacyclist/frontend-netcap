000e0f1249ab67c4e02d7abea0d2fa1dd2e94fc5
alohacyclist <c-mueller87@live.de>
2022-08-07 17:31:17 +0200

    add open close filter logic

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,7 +1,19 @@
 
 
-export function Filter() {
+export function Filter({openFilter, setOpenFilter}) {
   return (
-    <div>Filter</div>
+    <div
+    className="w-screen h-screen fixed top-0 left-0 bg-orange-400 rounded-lg z-10 p-3"
+    >
+      <div>
+        <h1
+        onClick={() => setOpenFilter(!openFilter)}
+        >Close
+        </h1>
+      </div>
+      
+      <h1>FILTER</h1>
+
+    </div>
   )
 }
