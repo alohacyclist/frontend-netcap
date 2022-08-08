949eb3dfd6439cd98c01067e1c9eec70985b987d
alohacyclist <c-mueller87@live.de>
2022-08-08 07:30:29 +0200

    add selected top-btn color logic

--- src/components/Elements/TopButtons.jsx
+++ src/components/Elements/TopButtons.jsx
@@ -1,21 +1,31 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCalendar, faPerson } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 
 export function TopButtons({topLeftButtonText, topCenterButtonText, topRightButtonText, switchResults, openFilter, setOpenFilter}) {
+  const [selected, setSelected] = useState(true)
 
   return (
     <span className="flex justify-between w-screen">
         <button 
-        className="m-4 border-2 border-lime-200 px-1 text-xs py-1 rounded-md"
-        onClick={() => switchResults(topLeftButtonText)}
+        className={`m-4 border-2 ${selected && 'bg-lime-200'}  px-1 text-xs py-1 rounded-md`}
+        disabled={selected}
+        onClick={() => {
+          setSelected(!selected)
+          switchResults(topLeftButtonText)
+        }}
         >
             <FontAwesomeIcon icon={faCalendar} className='mr-2'/>
             {topLeftButtonText}
         </button>
         <button 
-        className="m-4 border-2 border-lime-200 rounded-md px-1 text-xs py-1"
-        onClick={() => switchResults(topCenterButtonText)}
+        className={`m-4 border-2 ${!selected ? 'bg-lime-200' : 'border-lime-200'} px-1 text-xs py-1 rounded-md`}
+        disabled={!selected}
+        onClick={() => {
+          setSelected(!selected)
+          switchResults(topCenterButtonText)
+        }}
         >
             <FontAwesomeIcon icon={faPerson} className='mr-2'/>
             {topCenterButtonText}
