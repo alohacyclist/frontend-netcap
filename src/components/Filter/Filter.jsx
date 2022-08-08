79234f064c28fbb215b4b2a39c776736631dee44
alohacyclist <c-mueller87@live.de>
2022-08-08 08:18:30 +0200

    outlining filter radio btns

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,19 +1,91 @@
-
+import {age, experienceLevel, instructorLanguage} from '../../forms/filterInputs'
 
 export function Filter({openFilter, setOpenFilter}) {
   return (
     <div
     className="w-screen h-screen fixed top-0 left-0 bg-orange-400 rounded-lg z-10 p-3"
     >
-      <div>
+      <div className="flex flex-col w-screen p-3">
         <h1
         onClick={() => setOpenFilter(!openFilter)}
         >Close
         </h1>
-      </div>
       
       <h1>FILTER</h1>
+        
+        {experienceLevel.map((result) => (
+          <div  
+          key={result.id}
+          className='flex my-3 justify-between'
+          >
+            <label>{result.name}</label>
+            <input
+              id={result.id}
+              type={result.type}
+              name={result.category}
+              value={result.name}
+              /* onClick={() => {
+                result.category === 'location' ? 
+                  logLocation(result.category, result.name) :
+                  logActivity(result.category, result.name)
+              }} */
+            />
+          </div>
+        ))}
+        <div
+        className='flex flex-col flex-wrap h-52'
+        >
+        {instructorLanguage.map((result) => (
+          <div  
+          key={result.id}
+          className='flex my-3 pr-4'
+          >
+            <input
+              id={result.id}
+              type={result.type}
+              name={result.category}
+              value={result.name}
+              /* onClick={() => {
+                result.category === 'location' ? 
+                logLocation(result.category, result.name) :
+                logActivity(result.category, result.name)
+              }} */
+              />
+              <label
+              className='pl-4'
+              >{result.name}</label>
+          </div>
+        ))}
+        </div>
 
+        <div>
+          
+        </div>
+        <div>
+          
+        </div>
+        {age.map((result) => (
+          <div  
+          key={result.id}
+          className='flex my-3 justify-between'
+          >
+            <label>{result.name}</label>
+            <input
+              id={result.id}
+              type={result.type}
+              name={result.category}
+              value={result.name}
+              /* onClick={() => {
+                result.category === 'location' ? 
+                  logLocation(result.category, result.name) :
+                  logActivity(result.category, result.name)
+              }} */
+            />
+          </div>
+        ))}
+
+      </div>
+      
     </div>
   )
 }
---
+++ src/forms/filterInputs.js
@@ -0,0 +1,96 @@
+export const experienceLevel = [
+    {
+        id: "first-time",
+        type: "radio",
+        name: "First time",
+        category: 'experience'
+    },
+    {
+        id: "beginner",
+        type: "radio",
+        name: "Beginner",
+        category: 'experience'
+    },    {
+        id: "advanced",
+        type: "radio",
+        name: "Advanced",
+        category: 'experience'
+    },    {
+        id: "expert",
+        type: "radio",
+        name: "Expert",
+        category: 'experience'
+    }
+]
+
+export const instructorLanguage = [
+    {
+        id: "english",
+        type: "radio",
+        name: "English",
+        category: 'instructorLanguage'
+    },
+    {
+        id: "french",
+        type: "radio",
+        name: "French",
+        category: 'instructorLanguage'
+    },
+    {
+        id: "italian",
+        type: "radio",
+        name: "Italian",
+        category: 'instructorLanguage'
+    },
+    {
+        id: "dutch",
+        type: "radio",
+        name: "Dutch",
+        category: 'instructorLanguage'
+    },
+    {
+        id: "german",
+        type: "radio",
+        name: "German",
+        category: 'instructorLanguage'
+    },
+    {
+        id: "arabic",
+        type: "radio",
+        name: "Arabic",
+        category: 'instructorLanguage'
+    },
+    {
+        id: "spanish",
+        type: "radio",
+        name: "Spanish",
+        category: 'instructorLanguage'
+    },
+    {
+        id: "danish",
+        type: "radio",
+        name: "Danish",
+        category: 'instructorLanguage'
+    }
+]
+
+export const age = [
+    {
+        id: "adults",
+        type: "radio",
+        name: "Adults (19+)",
+        category: 'ageGroup'
+    },
+    {
+        id: "teens",
+        type: "radio",
+        name: "Teens (13-18)",
+        category: 'ageGroup'
+    },
+    {
+        id: "children",
+        type: "radio",
+        name: "Children (3-12)",
+        category: 'ageGroup'
+    }
+]
